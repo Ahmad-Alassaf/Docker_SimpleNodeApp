a5fa97124adb1de110cab59a923f7741ce2f6d4f
unknown <eng.ahmad.alassaf@gmail.com>
2025-12-27 10:30:44 +0100

    initial Project-- Express, dotenv,colors

---
+++ index.js
@@ -0,0 +1,8 @@
+const express=require('express')
+const dotenv=require('dotenv').config()
+const colors=require('colors')
+port=process.env.PORT||4000
+
+const app=express();
+app.get('/',(req,res)=>{res.send('<h1> Hello ! </br>Simple Nodejs App mit Docker </h1>')})
+app.listen(port,()=>console.log(`App running on Port : ${port}`.green.underline))
